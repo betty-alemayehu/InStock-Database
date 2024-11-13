4df224cf298b9fc62515aa9368f640892a2c9c26
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-13 15:37:51 -0500

    added basic validation checks with whitespace trimming

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -28,23 +28,21 @@ const createWarehouse = async (req, res) => {
 
   // Basic validation checks
   if (
-    !warehouse_name ||
-    !address ||
-    !city ||
-    !country ||
-    !contact_name ||
-    !contact_position ||
-    !contact_phone ||
-    !contact_email ||
+    !warehouse_name?.trim() ||
+    !address?.trim() ||
+    !city?.trim() ||
+    !country?.trim() ||
+    !contact_name?.trim() ||
+    !contact_position?.trim() ||
+    !contact_phone?.trim() ||
+    !contact_email?.trim() ||
     !/^\d{10}$/.test(contact_phone.replace(/\D/g, "")) || // Check for 10 digits
     !contact_email.includes("@") // Check for @ symbol
   ) {
-    return res
-      .status(400)
-      .json({
-        message:
-          "Invalid or missing data in request body, check for @ in email and only 10 numbers in phone field.",
-      });
+    return res.status(400).json({
+      message:
+        "Invalid or missing data in request body, check for @ in email and only 10 numbers in phone field.",
+    });
   }
 
   try {
