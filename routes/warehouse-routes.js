befae1a930135902cd563c19767cf3e4b79367cc
Michelle-Watson <alicerockshard@gmail.com>
2024-11-13 14:05:20 -0500

    Back-End: API to GET a Single Warehouse, added route and findOne logic in controller

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,6 +3,10 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 import express from "express";
 const router = express.Router();
 
+// Hit this route on "warehouses"
 router.route("/").get(warehouseController.index);
+
+// Hit this route on "warehouses/:id"
+router.route("/:id").get(warehouseController.findOne);
 
 export default router;
