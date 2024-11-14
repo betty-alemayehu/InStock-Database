f8b943e3c4fef77a110022f35d1b266a065462a2
nicolette-cheng <nikocheng@gmail.com>
2024-11-14 15:32:48 -0500

    add and export removeInventoryItem delete request func

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -174,4 +174,31 @@ const editInventoryItem = async (req, res) => {
   }
 };
 
-export { index, findOne, createInventoryItem, editInventoryItem };
+const removeInventoryItem = async (req, res) => {
+  try {
+    const itemDeleted = await knex("inventories")
+      .where({ id: req.params.id })
+      .delete();
+
+    if (!itemDeleted) {
+      return res
+        .status(404)
+        .json({ message: `Inventory item with ID ${req.params.id} not found` });
+    }
+
+    // No Content response
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to delete inventory item: ${error.message}`,
+    });
+  }
+};
+
+export {
+  index,
+  findOne,
+  createInventoryItem,
+  editInventoryItem,
+  removeInventoryItem,
+};
