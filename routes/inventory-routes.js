ae7a5c7093bd65d4f8a19c61d415f978efd868fb
nicolette-cheng <nikocheng@gmail.com>
2024-11-14 14:34:38 -0500

    refactor route paths to group under "/"

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -4,14 +4,12 @@ import express from "express";
 const router = express.Router();
 
 // GET - Hit this route on "inventories"
-router.route("/").get(inventoryController.index);
+router
+  .route("/")
+  .get(inventoryController.index)
+  .post(inventoryController.createInventoryItem)
+  .put(inventoryController.editInventoryItem);
 // GET - Hit this route on "inventories/:id"
 router.route("/:id").get(inventoryController.findOne);
-
-//POST - Add inventory
-router.route("/inventory/add").post(inventoryController.createInventoryItem);
-
-// PUT - edit inventory item
-router.route("/inventory/:id/edit").put(inventoryController.editInventoryItem);
 
 export default router;
