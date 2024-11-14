9f058a79655153554636d8f9d997ec62bbea70ea
Ife Onuorah <ifeonuorah@gmail.com>
2024-11-14 15:07:38 -0400

    Create basic routing for editing a warehouse

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 // GET - Hit this route on "warehouses"
 router.route("/").get(warehouseController.index);
 // GET - Hit this route on "warehouses/:id"
-router.route("/:id").get(warehouseController.findOne);
+router
+	.route("/:id")
+	.get(warehouseController.findOne)
+	.put(warehouseController.editWarehouse);
 //POST - Add warehouse
 router.route("/add").post(warehouseController.createWarehouse);
 
